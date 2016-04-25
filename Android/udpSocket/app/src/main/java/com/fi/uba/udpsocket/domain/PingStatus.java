36e2c902540779c54f392766057cdef9cbe5b29b
Adrian Duran <adrian.duran@fdvsolutions.com>
2016-04-25 19:18:51 -0300

    Use miliseconds instead of seconds to know when we must send all the data.

--- Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/domain/PingStatus.java
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/domain/PingStatus.java
@@ -66,7 +66,7 @@ public class PingStatus {
         Date now = TimeHelper.currentDate();
         float difference = now.getTime() - currentDate.getTime();
         Log.i("Ping Status", "Difference: "+String.valueOf(difference));
-        if ((difference/1000 >= 60) && longMessage) {
+        if ((difference >= 60000) && longMessage) {
             // We must check if the next is a long message, because in the other case, it could happen that a short message comes, and this values are reseted
             lastFileName = currentFilename;
             currentDate = now;
@@ -77,6 +77,4 @@ public class PingStatus {
             sendAllData = false;
         }
     }
-
-
 }
