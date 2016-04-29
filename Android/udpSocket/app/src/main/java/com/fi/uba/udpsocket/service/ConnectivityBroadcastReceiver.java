7af47452a966cba2ff191bed6e3dae1fc93f5ee2
Adrian Duran <adrian.duran@fdvsolutions.com>
2016-04-29 19:50:40 -0300

    Start with 3G connection handling.

---
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/service/ConnectivityBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.fi.uba.udpsocket.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by adrian on 4/29/16.
+ */
+public class ConnectivityBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("ConnectivityReceiver", "Response: ");
+
+    }
+}
