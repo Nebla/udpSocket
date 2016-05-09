4869fddc9f6d9972f2636922c2fa3ac40874c192
Adrian Duran <adrianmdu@gmail.com>
2016-05-09 00:04:36 -0300

    Launch udp servive when the device is turn on or the network connection is mobile.

--- Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/receiver/BootBroadcastReceiver.java
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/receiver/BootBroadcastReceiver.java
@@ -3,22 +3,31 @@ package com.fi.uba.udpsocket.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.fi.uba.udpsocket.R;
 import com.fi.uba.udpsocket.service.ServiceManager;
+import com.fi.uba.udpsocket.utils.Connectivity;
+import com.fi.uba.udpsocket.utils.PreferencesWrapper;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
 
+    private static final String logTag = BootBroadcastReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("BootBroadcastReceiver", "Boot complete recieved");
-        SharedPreferences prefs = context.getSharedPreferences("InstallationPreferences", Context.MODE_PRIVATE);
-        String installationName = prefs.getString("Installation", null);
+
+        Log.i(logTag, "Boot complete recieved");
+
+        String installationName = PreferencesWrapper.getInstallation(context);
+
         if (installationName != null) {
-            // Show the installation status
-            Log.i("BootBroadcastReceiver", "Installation "+installationName+" found");
-            ServiceManager.startService(context, installationName);
+            Log.i(logTag, "Installation "+installationName+" found");
+
+            if (Connectivity.isConnectedMobile(context)) {
+                Log.i(logTag, "MOBILE Connection");
+                ServiceManager.startService(context, installationName);
+            }
         }
         else {
             Log.i("BootBroadcastReceiver", "Installation NOT found");
--- Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/receiver/ConnectivityBroadcastReceiver.java
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/receiver/ConnectivityBroadcastReceiver.java
@@ -1,17 +1,52 @@
 package com.fi.uba.udpsocket.receiver;
 
+import com.fi.uba.udpsocket.service.ServiceManager;
+import com.fi.uba.udpsocket.utils.Connectivity;
+import com.fi.uba.udpsocket.utils.PreferencesWrapper;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 /**
  * Created by adrian on 4/29/16.
  */
 public class ConnectivityBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String logTag = ConnectivityBroadcastReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("ConnectivityReceiver", "Response: ");
+        Log.i(logTag, "Connectivity changed");
+
+        /*Log.i(logTag, "action: " + intent.getAction());
+        Log.i(logTag, "component: " + intent.getComponent());
+
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            for (String key: extras.keySet()) {
+                Log.i(logTag, "key [" + key + "]: " +
+                        extras.get(key));
+            }
+        }
+        else {
+            Log.i(logTag, "no extras");
+        }*/
+
+        if (Connectivity.isConnectedMobile(context)) {
+            Log.i(logTag, "MOBILE");
+            String installationName = PreferencesWrapper.getInstallation(context);
+            if (installationName != null) {
+                Log.i(logTag, "Installation "+installationName+" found");
+                ServiceManager.startService(context, installationName);
+            }
+        }
+        else {
+            Log.i(logTag, "NOT MOBILE");
+            ServiceManager.stopService(context);
+        }
 
     }
 }
