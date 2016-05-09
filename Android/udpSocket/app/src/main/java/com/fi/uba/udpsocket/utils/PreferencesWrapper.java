71fc2c81c93003f78c5e171455eb9a5deda83ac1
Adrian Duran <adrianmdu@gmail.com>
2016-05-09 00:00:40 -0300

    Implement the preferences wrapper needed methods.

--- Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/utils/PreferencesWrapper.java
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/utils/PreferencesWrapper.java
@@ -1,7 +1,51 @@
 package com.fi.uba.udpsocket.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.fi.uba.udpsocket.R;
+
 /**
  * Created by adrian on 4/18/16.
  */
 public class PreferencesWrapper {
+
+    public static void setInstallation(Context context, String installation) {
+        PreferencesWrapper.setString(context,
+                context.getString(R.string.preferences_name),
+                context.getString(R.string.installation_key),
+                installation);
+    }
+
+    public static String getInstallation(Context context) {
+        return PreferencesWrapper.getString(context,
+                context.getString(R.string.preferences_name),
+                context.getString(R.string.installation_key));
+    }
+
+    public static void removeInstallation(Context context) {
+        PreferencesWrapper.removeValue(context,
+                context.getString(R.string.preferences_name),
+                context.getString(R.string.installation_key));
+    }
+
+    public static void setString(Context context, String preferencesName, String key, String value) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(preferencesName, Context.MODE_PRIVATE).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String preferencesName, String key) {
+        SharedPreferences prefs = context.getSharedPreferences(preferencesName, Context.MODE_PRIVATE);
+        return prefs.getString(key, null);
+
+    }
+
+    public static void removeValue(Context context, String preferencesName, String key) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(preferencesName, Context.MODE_PRIVATE).edit();
+        editor.remove(key);
+        editor.apply();
+    }
+
+
 }
