2d3ff42342923439dcbe92f42ad6e63cf8be02be
Adrian Duran <adrianmdu@gmail.com>
2016-05-19 00:04:15 -0300

    Add buttons to start or stop service from current installation screen.

--- Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/screens/installations/CurrentInstallationActivity.java
+++ Android/udpSocket/app/src/main/java/com/fi/uba/udpsocket/screens/installations/CurrentInstallationActivity.java
@@ -1,15 +1,18 @@
 package com.fi.uba.udpsocket.screens.installations;
 
+import android.app.Service;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.fi.uba.udpsocket.R;
 import com.fi.uba.udpsocket.screens.login.LoginActivity;
 import com.fi.uba.udpsocket.service.ServiceManager;
+import com.fi.uba.udpsocket.utils.Connectivity;
 import com.fi.uba.udpsocket.utils.KeyManager;
 import com.fi.uba.udpsocket.utils.PreferencesWrapper;
 import com.fi.uba.udpsocket.utils.TimeLogHelper;
@@ -27,11 +30,32 @@ public class CurrentInstallationActivity extends ActionBarActivity {
 
         TextView installationTextView = (TextView) findViewById(R.id.installation_name);
         installationTextView.setText(installationName);
+
+        boolean isRunning = ServiceManager.isServiceRunning(getApplicationContext());
+        this.setButtonsState(isRunning);
+    }
+
+    private void setButtonsState(boolean isRunning) {
+        Button startButton = (Button) findViewById(R.id.start_service_action);
+        Button stopButton = (Button) findViewById(R.id.stop_service_action);
+        startButton.setEnabled(!isRunning);
+        stopButton.setEnabled(isRunning);
     }
 
     public void stopService(View view) {
+        ServiceManager.stopService(getApplicationContext());
+        this.setButtonsState(false);
     }
 
+    public void startService(View view) {
+        /*if (Connectivity.isConnectedMobile(getApplicationContext())) {
+            ServiceManager.startService(getApplicationContext(), installationName);
+            this.setButtonsState(true);
+        }
+        else {
+
+        }*/
+    }
 
     public void deleteCurrentInstallation(View view) {
         ServiceManager.stopService(getApplicationContext());
